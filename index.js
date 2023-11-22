c37fdade3ed99a798a91afda06ad01ba2f40e2ed
Raulheisser <144950061+Raulheisser@users.noreply.github.com>
2023-11-22 17:20:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+// Coleta e Análise de Dados do Usuário
+
+let nome;
+let cidade;
+
+
+console.log("Tipo da variável 'nome':", typeof nome);
+console.log("Tipo da variável 'cidade':", typeof cidade);
+
+
+nome = prompt("Digite seu nome:");
+cidade = prompt("Digite a cidade onde você mora:");
+
+
+console.log("Tipo da variável 'nome' após atribuição:", typeof nome);
+console.log("Tipo da variável 'cidade' após atribuição:", typeof cidade);
+
+
+console.log("Olá, meu nome é " + nome + " e eu moro em "+ cidade + ".");
+
+// Perguntas sim ou não
+
+let sim;
+let não;
+
+
+console.log("Tipo da variável 'sim':", typeof sim);
+console.log("Tipo da variável 'não':", typeof não);
+
+
+let = prompt("Você gosta de basquete ?");
+let = prompt("Você é saudável ?");
+let = prompt("Você gosta de dormir ?");
+
+
+
+// Multiplacação
+
+let x = 10
+let y = 5
+let z
+
+// Troque os valores dessas variáveis sem usar valores diretos
+z = x
+x = y
+y = z
+
+console.log("Troquei valores x = 10", x)
+console.log("Troquei os valores y = 5", y)
+
+// Imprima os novos valores de x e y
+print("Novo valor de x:", x)
+print("Novo valor de y:", y)
+
+let primeiroNumero = Number(promt("Insira seu primeiro número"))
+let segundoNumero = Number(promt("Insira seu segundo número"))
+
+let somar = primeiroNumero+segundoNumero
+console.log(somar)
+
+let multiplicar = primeiroNumero*segundoNumero
+
